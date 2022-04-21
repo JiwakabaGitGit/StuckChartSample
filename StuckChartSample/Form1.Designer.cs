ce1868fc96371af15727354dd196468eb50d2464
JiwakabaGitGit <jiwakaba.blog@gmail.com>
2022-04-22 08:06:22 +0900

    プロジェクト ファイルを追加します。

---
+++ StuckChartSample/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace StuckChartSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.LabelStyle.Enabled = false;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 90F;
+            chartArea1.Position.Width = 85F;
+            chartArea1.Position.X = 8F;
+            chartArea1.Position.Y = 10F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend1.Name = "Legend1";
+            legend1.Position.Auto = false;
+            legend1.Position.Height = 20F;
+            legend1.Position.Width = 100F;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar100;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1042, 63);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea2.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea2.AxisX.IsLabelAutoFit = false;
+            chartArea2.AxisX.IsStartedFromZero = false;
+            chartArea2.AxisX.LabelStyle.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea2.AxisX.LabelStyle.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea2.AxisX.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea2.AxisX.TitleAlignment = System.Drawing.StringAlignment.Near;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.Position.Auto = false;
+            chartArea2.Position.Height = 94F;
+            chartArea2.Position.Width = 100F;
+            chartArea2.Position.Y = 3F;
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(13, 111);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Light;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(1041, 327);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StuckChartSample/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace StuckChartSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializProductBar();
+
+            InitializeGraph();
+        }
+
+        private void InitializProductBar()
+        {
+            chart1.Series.Clear();
+
+            chart1.Series.Add( "ProductBar" );    //グラフ追加
+
+            // 凡例自体の幅と高さを設定します
+            chart1.Legends[ 0 ].Position.Width = 100.0f;
+            chart1.Legends[ 0 ].Position.Height = 15.0f;
+
+            // 位置をfloatで設定します(0～100)
+            chart1.Legends[ 0 ].Position.X = 0.0f;
+            chart1.Legends[ 0 ].Position.Y = 10.0f;
+
+            // ElementPositionオブジェクトを生成して代入しても設定できます。
+            // ElementPosition posi = new ElementPosition(70.0f, 80.0f, 20.0f, 20.0f);
+            // chart1.Legends[0].Position = posi;
+
+            // 凡例の位置などの自動設定を無効化します
+            chart1.Legends[ 0 ].Position.Auto = false;
+
+            //chart1.Legends[ 0 ].LegendStyle = LegendStyle.Row;
+
+            //var legend1 = new Legend
+            //{
+            //    //Alignment = StringAlignment.Far,
+            //    Docking = Docking.Top,
+            //    LegendStyle = LegendStyle.Row,
+            //};
+            //chart1.Legends.Add( legend1 );
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            #region
+            KindLog kind1 = new KindLog()
+            {
+                KindName = "A00000000000001",
+                StartTime = DateTime.Parse( "2022-01-01 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-01 23:59:59" )
+            };
+            KindLog kind2 = new KindLog()
+            {
+                KindName = "A00000000000002",
+                StartTime = DateTime.Parse( "2022-01-02 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-02 23:59:59" )
+            };
+            KindLog kind3 = new KindLog()
+            {
+                KindName = "A00000000000003",
+                StartTime = DateTime.Parse( "2022-01-03 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-03 23:59:59" )
+            };
+            KindLog kind4 = new KindLog()
+            {
+                KindName = "A00000000000004",
+                StartTime = DateTime.Parse( "2022-01-04 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-04 23:59:59" )
+            };
+            KindLog kind5 = new KindLog()
+            {
+                KindName = "A00000000000005",
+                StartTime = DateTime.Parse( "2022-01-05 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-05 23:59:59" )
+            };
+            KindLog kind6 = new KindLog()
+            {
+                KindName = "A00000000000006",
+                StartTime = DateTime.Parse( "2022-01-06 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-06 23:59:59" )
+            };
+            KindLog kind7 = new KindLog()
+            {
+                KindName = "A00000000000007",
+                StartTime = DateTime.Parse( "2022-01-07 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-07 23:59:59" )
+            };
+            KindLog kind8 = new KindLog()
+            {
+                KindName = "A00000000000008",
+                StartTime = DateTime.Parse( "2022-01-08 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-08 23:59:59" )
+            };
+            KindLog kind9 = new KindLog()
+            {
+                KindName = "A00000000000009",
+                StartTime = DateTime.Parse( "2022-01-09 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-09 23:59:59" )
+            };
+            KindLog kind10 = new KindLog()
+            {
+                KindName = "A00000000000010",
+                StartTime = DateTime.Parse( "2022-01-10 00:00:00" ),
+                EndTime = DateTime.Parse( "2022-01-10 23:59:59" )
+            };
+
+            List<KindLog> kindLogs = new List<KindLog>()
+            { kind1, kind2 };
+            //{ kind1, kind2, kind3, kind4 };
+            //{ kind1, kind2, kind3, kind4, kind5, kind6, kind7, kind8, kind9, kind10 };
+            //{ kind1, kind2, kind3, kind4, kind5, kind6, kind7 };
+            #endregion]
+
+            this.chart1.Series.Clear();
+
+            foreach ( var kindLog in kindLogs )
+            {
+                chart1.Series.Add( kindLog.KindName );    //グラフ追加
+                                              //グラフの種類を指定（Columnは積み上げ縦棒グラフ）
+                chart1.Series[ kindLog.KindName ].ChartType = SeriesChartType.StackedBar100;
+                chart1.Series[ kindLog.KindName ].LegendText = kindLog.KindName;  //凡例に表示するテキストを指定
+            }
+
+            foreach ( var kindLog in kindLogs )
+            {
+                DataPoint dp = new DataPoint();
+                TimeSpan time = kindLog.EndTime - kindLog.StartTime;
+                dp.SetValueXY( "ProductBar", time.TotalSeconds );  //XとYの値を設定
+                dp.IsValueShownAsLabel = true;  //グラフに値を表示するように指定
+                chart1.Series[ kindLog.KindName ].Points.Add( dp );   //グラフにデータ追加
+            }
+        }
+
+        private void InitializeGraph()
+        {
+            // 初期化
+            chart2.Series.Clear();
+            chart2.ChartAreas.Clear();
+
+            // chart設定
+            string Name1 = "Bar1";
+            string AreaName1 = "chartArea1";
+            chart2.Series.Add( Name1 );// Series追加
+            chart2.ChartAreas.Add( AreaName1 );
+            chart2.Series[ Name1 ].ChartType = SeriesChartType.Column;
+
+            chart2.ChartAreas[ 0 ].AxisX.IntervalOffset = 0;
+            chart2.ChartAreas[ 0 ].AxisX.Interval = 1;
+            chart2.ChartAreas[ 0 ].AxisX.IntervalOffsetType = DateTimeIntervalType.Days;
+            //max, min 目盛設定
+            //chart2.ChartAreas[ AreaName1 ].AxisY.Maximum = 100;
+            //chart2.ChartAreas[ AreaName1 ].AxisY.Minimum = 0;
+            //chart2.ChartAreas[ AreaName1 ].AxisY.Interval = 20;
+
+            // 凡例自体の幅と高さを設定します
+            chart2.Legends[ 0 ].Position.Width = 100.0f;
+            chart2.Legends[ 0 ].Position.Height = 15.0f;
+
+            // 位置をfloatで設定します(0～100)
+            chart2.Legends[ 0 ].Position.X = 0.0f;
+            chart2.Legends[ 0 ].Position.Y = 95.0f;
+
+            // 凡例の位置などの自動設定を無効化します
+            chart2.Legends[ 0 ].Position.Auto = false;
+
+            // 目盛線ちょっと出てるところがなくなる
+            //chart2.ChartAreas[ AreaName1 ].AxisX.MajorTickMark.Enabled = false;
+
+            // X軸のフォーマットを設定します
+            chart2.ChartAreas[ 0 ].AxisX.LabelStyle.Format = "yyyy/MM/dd";
+            chart2.ChartAreas[ 0 ].AxisX.LabelStyle.Angle = 90;
+            
+
+            DateTime baseDt = DateTime.Today;
+            baseDt.AddHours( 12 );
+            chart2.Series[ Name1 ].XValueType = ChartValueType.Date;
+
+            // データ設定
+            for ( int i = 0; i < 1; i++ )
+            {
+                DateTime tempDt = baseDt.AddDays( i );
+                chart2.Series[ Name1 ].Points.AddXY( tempDt.ToOADate(), (i+1) * 1 );
+            }
+        }
+    }
+}
